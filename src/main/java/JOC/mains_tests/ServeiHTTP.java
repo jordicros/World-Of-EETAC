5fae669d90972e8dccf1db3fa166cf6315a69da0
marcllena <36449401+marcllena@users.noreply.github.com>
2018-04-23 19:28:50 +0200

    Afagir Codis d'error als primers serveis

--- src/main/java/JOC/mains_tests/ServeiHTTP.java
+++ src/main/java/JOC/mains_tests/ServeiHTTP.java
@@ -24,13 +24,26 @@ public class ServeiHTTP {
     public ServeiHTTP() throws IOException{
         mon = Dades.getInstance().getMundo();
     }
+    /*
     @GET
     @Path("/escenaris/{id}")
     @Produces(MediaType.APPLICATION_JSON )
     public Escena enviarEscenario(@PathParam("id") int id){
         return mon.obtenerEscena(id);
 
+    }*/
+    @GET
+    @Path("/escenaris/{id}")
+    public Response enviarEscenario(@PathParam("id") int id){
+        Escena escena=mon.obtenerEscena(id);
+        if(escena!=null){
+            return Response.ok(escena,MediaType.APPLICATION_JSON).build();
+        }
+        else {
+            return Response.status(204).build();
+        }
     }
+    /*
     @GET
     @Path("/usuari/reg/{nom}/{pass}/{proff}")
     @Produces (MediaType.APPLICATION_JSON)
@@ -39,12 +52,37 @@ public class ServeiHTTP {
         mon.crearUsuario(temp);
         return mon.consultarUsuario(nom);
     }
+    */
+    @GET
+    @Path("/usuari/reg/{nom}/{pass}/{proff}")
+    public Response regUsuario(@PathParam("nom")String nom,@PathParam("pass") String pass,@PathParam("proff") int proff){
+        Usuario temp = new Usuario( nom, pass, proff);
+        boolean res =mon.crearUsuario(temp);
+        if(res){
+            return Response.ok(mon.consultarUsuario(nom),MediaType.APPLICATION_JSON).build();
+        }
+        else {
+            return Response.status(204).build();
+        }
+    }
+    /*
     @GET
     @Path("/usuari/{nombre}")
     @Produces(MediaType.APPLICATION_JSON) //NO RESPON, UNA FUNCIO SENZILLA SI QUE TIRA
     public Usuario enviarUsuarioJSON(@PathParam("nombre") String nombre) {
         Usuario temp = mon.consultarUsuario(nombre);
         return temp;
+    }*/
+    @GET
+    @Path("/usuari/{nombre}")
+    public Response enviarUsuarioJSON(@PathParam("nombre") String nombre) {
+        Usuario temp = mon.consultarUsuario(nombre);
+        if(temp!=null){
+            return Response.ok(temp,MediaType.APPLICATION_JSON).build();
+        }
+        else {
+            return Response.status(204).build();
+        }
     }
     /*@GET
     @Path("/getEscenas")
